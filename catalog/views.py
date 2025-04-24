bfa828652eb670d93ccf75e35edd435a9fcc83b4
oksanafedorova07 <kcenka4@gmail.com>
2025-04-24 11:44:38 +0300

    corect home, views.py

--- catalog/views.py
+++ catalog/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def contacts(request):
-    return render(request, 'catalog/contacts.html')
 
 def home(request):
     return render(request, 'catalog/home.html')
