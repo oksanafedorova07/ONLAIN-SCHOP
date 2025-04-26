906dfd2fea440e25e2c24479e4b75d616ca8b72c
oksanafedorova07 <kcenka4@gmail.com>
2025-04-27 02:24:34 +0300

    changes made admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 
-from .models import Category, Product
+from .models import Category, Product, Contact
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -12,3 +12,9 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price', 'category')
     list_filter = ('category',)
     search_fields = ('name', 'description')
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'created_at')  # Все поля должны быть в модели!
+    list_filter = ('created_at',)
+    search_fields = ('name', 'email', 'message')
