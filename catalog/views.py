9ef253c29a9b8543f65e00754ae45e0b9bd8c418
oksanafedorova07 <kcenka4@gmail.com>
2025-04-27 01:29:30 +0300

    add views.py

--- catalog/views.py
+++ catalog/views.py
@@ -7,7 +7,7 @@ def home(request):
     return render(request, 'catalog/home.html')
 
 
-def contact(request):
+def contacts(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
