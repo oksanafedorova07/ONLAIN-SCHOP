28ed7dbf1afab199d459f40cc1d79eda6aea88ec
oksanafedorova07 <kcenka4@gmail.com>
2025-04-27 23:04:22 +0300

    changes made fail

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.translation import gettext_lazy as _
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name="Название")
